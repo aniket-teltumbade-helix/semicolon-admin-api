f0a7336136dddb9d8916555e542919223ef3ebcc
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2022-02-16 17:18:21 +0530

    fix: System paths removed from API responses

--- src/helpers/node-compiler.interceptor.ts
+++ src/helpers/node-compiler.interceptor.ts
@@ -1,21 +1,22 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import path from 'path';
+import * as path from 'path';
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { map, Observable } from 'rxjs';
 import { spawnSync } from 'child_process';
 import { errorMessage } from 'src/error';
-import { v4 as v4uuid } from 'uuid'; 'uuid'
+import { v4 as v4uuid } from 'uuid';
 
 @Injectable()
 export class NodeCompilerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     let request = context.switchToHttp().getRequest();
+    var dir = path.join(__dirname, '..', 'scripts');
+    var vid = v4uuid()
     try {
-      var dir = path.join(__dirname, '..', 'scripts');
       if (!existsSync(dir)) {
         mkdirSync(dir);
       }
-      var fileName = path.join(dir, `${v4uuid()}.js`)
+      var fileName = path.join(dir, `${vid}.js`)
       writeFileSync(fileName, request.body?.script)
       const scriptExecution = spawnSync('node', [fileName], {
         input: request.body?.input,
@@ -24,9 +25,11 @@ export class NodeCompilerInterceptor implements NestInterceptor {
       if (scriptExecution.status === 0) {
         return next.handle().pipe(map(flow => flow.data = { message: scriptExecution.stdout.toString().trim() }))
       } else {
-        return next.handle().pipe(map(flow => flow.data = errorMessage('INTERNAL_SERVER_ERROR', scriptExecution.stderr.toString().trim())))
+        let error = scriptExecution.stderr.toString().trim().replace(`${dir}\\${vid}`, 'index')
+        return next.handle().pipe(map(flow => flow.data = errorMessage('INTERNAL_SERVER_ERROR', error)))
       }
     } catch (error) {
+      error = error.replace(`${dir}\\${vid}`, 'index')
       return next.handle().pipe(map(flow => flow.data = errorMessage('INTERNAL_SERVER_ERROR', `${error}`)));
     }
   }
