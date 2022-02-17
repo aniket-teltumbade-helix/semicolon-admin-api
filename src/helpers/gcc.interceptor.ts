1a2b91c336c06c832dabb1dc73d4e189ac27217e
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2022-02-17 14:36:25 +0530

    API for gcc compiler added

---
+++ src/helpers/gcc.interceptor.ts
@@ -0,0 +1,54 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import * as path from 'path';
+import { execSync, spawnSync } from 'child_process';
+import { errorMessage } from 'src/error';
+import { v4 as v4uuid } from 'uuid'
+@Injectable()
+export class GccInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    let request = context.switchToHttp().getRequest();
+
+    var dir1 = path.join(__dirname, '..', 'scripts');
+    var uusidString = v4uuid()
+    var dir = path.join(__dirname, '..', 'scripts', uusidString.slice(0, uusidString.indexOf("-")));
+    try {
+      var className = 'Solution'
+      if (!existsSync(dir1)) {
+        mkdirSync(dir1);
+        mkdirSync(dir);
+      }
+      else {
+        mkdirSync(dir);
+      }
+      var fileName = path.join(dir, className)
+      writeFileSync(`${fileName}.c`, request.body?.script)
+      // writeFileSync(`${fileName}.txt`, )
+      request.body?.input
+      let scriptCompile = spawnSync('gcc', [`${fileName}.c`, '-o', fileName], {
+        timeout: 5000
+      })
+
+      if (scriptCompile.status === 0) {
+        let scriptExecution = spawnSync(fileName, {
+          input: request.body?.input,
+          timeout: 1000
+        })
+        if (scriptExecution.status === 0) {
+          return next.handle().pipe(map(flow => flow.data = { message: scriptExecution.stdout.toString().trim() }))
+        } else {
+          let error = scriptExecution.stderr.toString().split(`${dir}\\`).join('')
+          return next.handle().pipe(map(flow => flow.data = errorMessage('ACCEPTED', error)))
+        }
+      } else {
+        let error = scriptCompile.stderr.toString().split(`${dir}\\`).join('')
+        return next.handle().pipe(map(flow => flow.data = errorMessage('ACCEPTED', error)))
+      }
+
+    } catch (error) {
+      error = `${error}`.split(`${dir}\\`).join('')
+      return next.handle().pipe(map(flow => flow.data = errorMessage('INTERNAL_SERVER_ERROR', `${error}`)));
+    }
+  }
+}
